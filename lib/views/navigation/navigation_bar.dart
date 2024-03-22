98ae6bc5231a1de071a196040c121be19347dee0
Anup Kolhe <69685239+AnupKolhe@users.noreply.github.com>
2024-03-22 22:52:57 +0530

    added Nav Bar
    Added Nav bar

---
+++ lib/views/navigation/navigation_bar.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class NavigationBarPage extends StatefulWidget {
+  const NavigationBarPage({super.key});
+
+  @override
+  State<NavigationBarPage> createState() => _NavigationBarPageState();
+}
+
+class _NavigationBarPageState extends State<NavigationBarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Row(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Text('Home'),
+              Text('About'),
+              Text('Projects'),
+              Text('Contacts'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
